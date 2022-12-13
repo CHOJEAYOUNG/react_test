0c7a633965c506bba2f385aea65a25130d9997a6
CHOJEAYOUNG <wodudgha@naver.com>
2022-12-13 22:12:02 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const ther= "ther";
+// const fir = "fir";
+
+// const arrayt = ["mon","tue","wed","ther","fir"];
+
+// console.log(arrayt);
+
+// arrayt.push("dddfsdfs");
+
+
+// console.log(arrayt);
+
+// for(var i=0; i<=5; i++){
+//     console.log(arrayt[i]);
+// }
+
+
+// const toBuy = ["potato","tt","bb"];
+
+// console.log(toBuy[1]);
+
+
+// const playerName = "cho";
+// const playerPoint = 222;
+// const playerHandsome = false;
+// const palyerFat = "little bit";
+
+// const player = {
+//     name:"cho",
+//     points:10,
+//     fat:true
+// };
+// player.lastName = "ptoto";
+
+// player.points = player.points+10;
+// console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+
+
+
+// player.fat = true;
+
+
+// console.log("helllo world~~~~~~00");
+// console.log("helllo world~~~~~~11");
+// console.log("helllo world~~~~~~22");
+// console.log("helllo world~~~~~~33");
+// console.log("helllo world~~~~~~44");
+// console.log("helllo world~~~~~~55");
+
+
+// function sayHello(dd){
+//     console.log("hello~" + dd);
+// }
+
+// alert("");
+
+// sayHello("rrr");
+// sayHello("dfd");
+// sayHello("qweqwe");
+
+
+
+// function plus(a,b){
+//     console.log(a + b);
+// }
+
+
+// function divide(a,b){
+//     console.log(a/b);
+// }
+// plus(8, 60);
+
+
+const player = {
+    name: "cho",
+    point:50,
+    fat:true,
+    sayHello:function(otherPat){
+        console.log("hello"+ otherPat+" ///dfd");
+    }
+}
+
+console.log(player.name);
+player.sayHello("meee");
